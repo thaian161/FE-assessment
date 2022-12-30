eb7202c2f2d4e749d00b8952f5484cd0f9bcdbc9
thaian161 <thaian161@yahoo.com>
2022-12-30 03:08:58 -0600

    show pop up on click

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import { TiThMenu } from 'react-icons/ti';
 import TextPopUp from './components/PopUp/TextPopUp';
 import DatePopUp from './components/PopUp/DatePopUp';
 import ProgressPopUp from './components/PopUp/ProgressPopUp';
+import { useState } from 'react';
 import './App.css';
 
 // import { useState, useEffect } from 'react';
 
 function App() {
+  const [showMessage, setShowMessage] = useState(false);
+  const [showProgress, setShowProgress] = useState(false);
+  const [showDate, setShowDate] = useState(false);
   return (
     <>
       <div className="Nav">
@@ -17,14 +21,15 @@ function App() {
       </div>
 
       <section className="body">
-        <button> Message </button>
-        <button> Date </button>
-        <button> Progress </button>
-      </section>
+        <button onClick={() => setShowMessage(!showMessage)}> Message </button>
+        {showMessage ? <TextPopUp /> : null}
 
-      <TextPopUp />
-      <ProgressPopUp />
-      <DatePopUp />
+        <button onClick={() => setShowProgress(!showDate)}> Date </button>
+        {showDate ? <DatePopUp /> : null}
+
+        <button onClick={() => setShowDate(!showProgress)}> Progress </button>
+        {showProgress ? <ProgressPopUp /> : null}
+      </section>
 
       <section>
         <img src={polySense} className="polysense" alt="polysense img" />
