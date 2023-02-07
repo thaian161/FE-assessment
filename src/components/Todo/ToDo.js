cdc1183d0b11878b764e2180c202365e15f7fece
thaian161 <thaian161@yahoo.com>
2023-02-07 00:20:37 -0600

    save todo in local storage

--- src/components/Todo/ToDo.js
+++ src/components/Todo/ToDo.js
@@ -3,14 +3,28 @@ import './ToDo.css';
 import { useState } from 'react';
 
 function ToDo() {
+  const storageJobs = localStorage.getItem('jobs') //this is JSON format, we need to parse it so the data type returns back to array - use JSON.parse(storageJobs)
+
+  console.log(JSON.parse(storageJobs))
 
   const [job, setJob] = useState("");
   const [jobs, setJobs] = useState([]);
   console.log(job)
 
   const handleSubmit= () => {
-    setJobs(prev => [...prev, job]);
+    setJobs(prev => {
+      const newJobs = [...prev,job]
+
+      //save to local storage - check in Application > local storage
+      const jsonJobs = JSON.stringify(newJobs)
+      console.log(jsonJobs)
+      localStorage.setItem("jobs", jsonJobs)
+
+      return newJobs
+    } );
     setJob('') // clear input after user hit submit button
+
+    console.log(jobs)
   }
 
   return (
