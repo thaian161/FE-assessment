0dca3734e2f5a271c9f54c6a3521f07adf64dbc2
Ann Bui <thaian161@yahoo.com>
2023-05-02 12:13:35 -0500

    Refactor from useState hook to useReducer hook

--- src/components/PracticeReducer/PracticeReducer.js
+++ src/components/PracticeReducer/PracticeReducer.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
+
+const reducerFunction = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { count: state.count + 1, showText: state.showText };
+
+    case 'toggleShowText':
+      return { count: state.count, showText: !state.showText };
+
+    default:
+      return state;
+  }
+};
 
 function PracticeReducer() {
-  const [count, setCount] = useState(0);
-  const [showText, setShowText] = useState(true);
+  // useState method
+  // const [count, setCount] = useState(0);
+  // const [showText, setShowText] = useState(true);
+
+  //useReducer method
+  const [state, dispatch] = useReducer(reducerFunction, {
+    // initial state
+    count: 0,
+    showText: true,
+  });
 
   return (
     <>
-      <h1> {count} </h1>
+      <h1> {state.count} </h1>
 
       <button
         onClick={() => {
-          setCount(count + 1);
-          setShowText(!showText);
+          // setCount(count + 1);
+          // setShowText(!showText);
+          dispatch({ type: 'INCREMENT' });
+          dispatch({ type: 'toggleShowText' });
         }}
       >
         Click Me
       </button>
 
-      {showText && <p>This is a test for showText state</p>}
+      {state.showText && <p>This is a test for showText state</p>}
     </>
   );
 }
